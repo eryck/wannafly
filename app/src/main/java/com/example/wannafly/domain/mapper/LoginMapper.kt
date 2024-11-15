09ee7065fd601547a4c19d031cb5c97e037d5443
Gabriel Botao <gabriel.botao@br.experian.com>
2024-11-15 10:48:46 -0300

    feature(MVVM): create domain layer

---
+++ app/src/main/java/com/example/wannafly/domain/mapper/LoginMapper.kt
@@ -0,0 +1,5 @@
+package com.example.wannafly.domain.mapper
+
+class LoginMapper {
+    // TODO: implement fun to parse response to model
+}
---
+++ app/src/main/java/com/example/wannafly/domain/model/LoginModel.kt
@@ -0,0 +1,6 @@
+package com.example.wannafly.domain.model
+
+data class LoginModel(
+    // TODO: implement model
+    val data: String
+)
