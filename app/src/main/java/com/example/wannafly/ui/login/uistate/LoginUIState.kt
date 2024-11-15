a8d32c46f978a90d904035b3676bbf42f0504de9
Gabriel Botao <gabriel.botao@br.experian.com>
2024-11-15 10:48:56 -0300

    feature(MVVM): create ui layer

---
+++ app/src/main/java/com/example/wannafly/ui/login/uistate/LoginUIState.kt
@@ -0,0 +1,5 @@
+package com.example.wannafly.ui.login.uistate
+
+sealed class LoginUIState {
+    // TODO: states of screen (Loading, Success, Error...)
+}
---
+++ app/src/main/java/com/example/wannafly/ui/login/view/LoginScreen.kt
@@ -0,0 +1,8 @@
+package com.example.wannafly.ui.login.view
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun LoginScreen() {
+    // TODO: implement layout
+}
