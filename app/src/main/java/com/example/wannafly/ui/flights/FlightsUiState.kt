7d9a2fca440d74d596fee34d19ed4d821d517edf
Raul Espim <oraulespim@gmail.com>
2024-11-17 20:45:14 -0300

    replace to sealed interface FlightsUiState

--- app/src/main/java/com/example/wannafly/ui/flights/FlightsUiState.kt
+++ app/src/main/java/com/example/wannafly/ui/flights/FlightsUiState.kt
@@ -3,8 +3,8 @@ package com.example.wannafly.ui.flights
 import com.example.wannafly.common.UiText
 import com.example.wannafly.domain.model.Flight
 
-sealed class FlightsUiState {
-    data class Success(val flights: List<Flight>) : FlightsUiState()
-    data object Loading : FlightsUiState()
-    data class TryAgain(val errorMessage: UiText?) : FlightsUiState()
+sealed interface FlightsUiState {
+    data class Success(val flights: List<Flight>) : FlightsUiState
+    data object Loading : FlightsUiState
+    data class TryAgain(val errorMessage: UiText?) : FlightsUiState
 }
