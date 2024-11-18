12381cac9c31a7341f80b94dbeee1d4f96f03521
Gabriel Botao <gabriel.botao@br.experian.com>
2024-11-18 10:55:51 -0300

    feature(Screen): inflate login first

--- app/src/main/java/com/example/wannafly/MainActivity.kt
+++ app/src/main/java/com/example/wannafly/MainActivity.kt
@@ -4,14 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.wannafly.ui.login.view.LoginScreen
 import com.example.wannafly.ui.theme.WannaFlyTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,29 +18,10 @@ class MainActivity : ComponentActivity() {
         installSplashScreen()
         setContent {
             WannaFlyTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold { innerPadding ->
+                    LoginScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    WannaFlyTheme {
-        Greeting("Android")
     }
 }
