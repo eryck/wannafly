f6c623492e792ecf6e6810168db0915c41085517
Gabriel Botao <gabriel.botao@br.experian.com>
2024-11-20 14:59:00 -0300

    feature(Screen): remove import not use

--- app/src/main/java/com/example/wannafly/ui/profile/ProfileScreen.kt
+++ app/src/main/java/com/example/wannafly/ui/profile/ProfileScreen.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardElevation
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
